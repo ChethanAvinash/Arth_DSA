7a62b0bdb811fc8899ddc7de41e602e66d3e9a74
ChethanAvinash <chethan.avinash45@gmail.com>
2021-06-21 22:19:54 +0530

    Updated Day4 Q4

--- Recursion/Day-4 (20-06-2021)/Ambiguous Measurements/Ambiguous_Measurements.py
+++ Recursion/Day-4 (20-06-2021)/Ambiguous Measurements/Ambiguous_Measurements.py
@@ -17,3 +17,47 @@ cups = [[100,200],[450,465],[800,850]]
 lower = 2100
 upper = 2300
 print(canMeasure(cups,len(cups)-1,lower,upper,0,0))
+
+# Approach-2 ( with memoization )
+
+def canMeasureDP(cups,lower,upper,dp):
+    '''
+        A list or a matrix can't be used for DP because the limits are unknown
+        i.e we are given with only the ranges hence a fixed DP array is not
+        possible. Therefore a hashmap is a good option to store all the 
+        possible lower ans upper levels and a unique key is assigned that is 
+        based on the lower and upper values itself.
+
+    '''
+    _key = str(lower) + ":" + str(upper)
+
+    # if there is a value for the given key in the hashmap then we have to 
+    # return the pre-computed value.
+
+    if _key in dp:
+        return dp[_key]
+    
+    if lower<=0 and upper<=0:
+        # we have exceeded
+        return False
+    
+    canMeasure = False
+    for cup in cups:
+        cupLow,cupHigh = cup
+        if lower<=cupLow and cupHigh<=upper:
+            canMeasure = True
+            break
+        newLow = max(0,lower-cupLow)
+        newHigh = max(0,upper-cupHigh)
+        canMeasure = canMeasureDP(cups,newLow,newHigh,dp)
+        if canMeasure:
+            break
+    dp[_key] = canMeasure
+    return canMeasure
+
+    
+cups = [[100,200],[450,465],[800,850]]
+lower = 2100
+upper = 2300
+dp = {}
+print(canMeasureDP(cups,lower,upper,dp))
